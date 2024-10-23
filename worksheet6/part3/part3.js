27228ba42ad4f934e309de15622273cd7c99ed58
trgao <112098096+trgao@users.noreply.github.com>
2024-10-23 16:26:17 +0200

    Add txt explanation files

--- worksheet6/part3/part3.js
+++ worksheet6/part3/part3.js
@@ -62,11 +62,11 @@ window.onload = function init() {
         var texture = gl.createTexture();
         gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-        gl.generateMipmap( gl.TEXTURE_2D );
+        gl.generateMipmap(gl.TEXTURE_2D );
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-        gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,gl.LINEAR_MIPMAP_LINEAR);
-        gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,gl.LINEAR_MIPMAP_LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     };
     image.src = 'earth.jpg';
     gl.uniform1i(gl.getUniformLocation(program, 'texMap'),0);
